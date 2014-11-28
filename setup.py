7d275fd30e938947990bffa73e321caff611bcd3
Christoph Wehmeyer <christoph.wehmeyer@fu-berlin.de>
2014-11-28 23:01:59 +0100

    setup and help files

---
+++ setup.py
@@ -0,0 +1,61 @@
+from setuptools import setup
+from distutils.core import Extension
+from Cython.Distutils import build_ext
+import numpy as np
+
+ext_dtram = Extension(
+        "pytram.dtram.ext",
+        sources=["ext/dtram/dtram.pyx", "ext/dtram/_dtram.c" ],
+        include_dirs=[np.get_include()],
+        extra_compile_args=["-O3"]
+    )
+
+setup(
+    cmdclass={'build_ext': build_ext},
+    ext_modules=[
+            ext_dtram
+        ],
+    name='pytram',
+    version='0.1.0',
+    description='The TRAM package',
+    long_description='The python interface to the TRAM framework for estimating Markov state models from biased MD simulations.',
+    classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Environment :: Console',
+            'Intended Audience :: Science/Research',
+            'License :: OSI Approved :: BSD License',
+            'Natural Language :: English',
+            'Operating System :: MacOS :: MacOS X',
+            'Operating System :: POSIX :: Linux',
+            'Programming Language :: C',
+            'Programming Language :: Cython',
+            'Programming Language :: Python :: 2.7',
+            'Topic :: Scientific/Engineering :: Bio-Informatics',
+            'Topic :: Scientific/Engineering :: Chemistry',
+            'Topic :: Scientific/Engineering :: Mathematics',
+            'Topic :: Scientific/Engineering :: Physics'
+        ],
+    keywords=[
+            'free energy',
+            'Markov state model',
+            'TRAM',
+            'dTRAM'
+        ],
+    url='https://github.com/markovmodel/pytram',
+    author='The pytram team',
+    author_email='pytram@lists.fu-berlin.de',
+    license='Simplified BSD License',
+    setup_requires=[
+            'numpy>=1.7.1',
+            'cython>=0.15',
+            'setuptools>=0.6'
+        ],
+    tests_require=[ 'numpy>=1.7.1', 'nose>=1.3.4' ],
+    install_requires=[ 'numpy>=1.7.1' ],
+    packages=[
+            'pytram',
+            'pytram.dtram'
+        ],
+    test_suite='test',
+    scripts=[]
+)
