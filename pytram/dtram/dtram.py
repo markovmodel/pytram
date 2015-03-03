5013f87bd7a23fc1a9720bed0ba625cddd844a05
Christoph Wehmeyer <christoph.wehmeyer@fu-berlin.de>
2015-03-03 22:57:44 +0100

    [dTRAM] using tag switch

--- pytram/dtram/dtram.py
+++ pytram/dtram/dtram.py
@@ -25,7 +25,7 @@ class DTRAM( Estimator ):
     r"""
     The dTRAM estimator class
     """
-    def __init__( self, C_K_ij, b_K_i ):
+    def __init__( self, C_K_ij, b_K_i, tagged=True ):
         r"""
         Initialize the DTRAM object
         
@@ -37,15 +37,19 @@ class DTRAM( Estimator ):
             bias energies in the T thermodynamic and M discrete Markov states
         """
         super( DTRAM, self ).__init__( C_K_ij )
-        # this check raises an exception if b_K_i is not usable
-        if self._check_b_K_i( b_K_i ):
-            self._gamma_K_i = np.exp( b_K_i.min() - b_K_i )
-        # hard-coded initial guess for pi_i and nu_K_i
-        self._pi_i = np.ones( shape=(self.n_markov_states,), dtype=np.float64 ) / float( self.n_markov_states )
-        self._nu_K_i = C_K_ij.sum( axis=2 ).astype( np.float64 )
-        # 'private' storage variable initialization
-        self._f_K = None
-        self._pi_K_i = None
+        self.tagged = tagged
+        if tagged:
+            # this check raises an exception if b_K_i is not usable
+            if self._check_b_K_i( b_K_i ):
+                self._gamma_K_i = np.exp( b_K_i.min() - b_K_i )
+            # hard-coded initial guess for pi_i and nu_K_i
+            self._pi_i = np.ones( shape=(self.n_markov_states,), dtype=np.float64 ) / float( self.n_markov_states )
+            self._nu_K_i = C_K_ij.sum( axis=2 ).astype( np.float64 )
+            # 'private' storage variable initialization
+            self._f_K = None
+            self._pi_K_i = None
+        else:
+            pass
         # citation information
         self.citation = [
                 "Statistically optimal analysis of state-discretized trajectory data",
@@ -62,23 +66,35 @@ class DTRAM( Estimator ):
 
     @property
     def pi_i( self ):
-        return self._pi_i
+        if self.tagged:
+            return self._pi_i
+        else:
+            return None
 
     @property
     def pi_K_i( self ):
-        if self._pi_K_i is None:
-            self._pi_K_i = self.f_K[:,np.newaxis] * self.pi_i[np.newaxis,:] * self.gamma_K_i
-        return self._pi_K_i
+        if self.tagged:
+            if self._pi_K_i is None:
+                self._pi_K_i = self.f_K[:,np.newaxis] * self.pi_i[np.newaxis,:] * self.gamma_K_i
+            return self._pi_K_i
+        else:
+            return None
 
     @property
     def f_K( self ):
-        if self._f_K is None:
-            self._f_K = 1.0 / np.dot( self.gamma_K_i, self.pi_i )
-        return self._f_K
+        if self.tagged:
+            if self._f_K is None:
+                self._f_K = 1.0 / np.dot( self.gamma_K_i, self.pi_i )
+            return self._f_K
+        else:
+            return None
 
     @property
     def nu_K_i( self ):
-        return self._nu_K_i
+        if self.tagged:
+            return self._nu_K_i
+        else:
+            return None
 
     ############################################################################
     #
@@ -99,36 +115,39 @@ class DTRAM( Estimator ):
         verbose : boolean
             writes convergence information to stdout during the self-consistent-iteration cycle
         """
-        # reset internal storage variables
-        self._f_K = None
-        self._pi_K_i = None
-        finc = None
-        if verbose:
-            print "# %25s %25s" % ( "[iteration step]", "[relative increment]" )
-        # start the iteration loop
-        for i in xrange( maxiter ):
-            # iterate nu_K_i
-            tmp_nu_K_i = np.copy( self.nu_K_i )
-            nu_K_i_equation_tagged( tmp_nu_K_i, self.gamma_K_i, self.pi_i, self.C_K_ij, self._nu_K_i )
-            # iterate pi_i
-            tmp_pi_i = np.copy( self.pi_i )
-            pi_i_equation_tagged( self.nu_K_i, self.gamma_K_i, tmp_pi_i, self.C_K_ij, self._pi_i )
-            # normalize pi_i
-            self._pi_i /= self.pi_i.sum()
-            # compute the relative change of pi_i
-            nonzero = tmp_pi_i.nonzero()
-            div = np.ones( shape=(self.n_markov_states,), dtype=np.float64 )
-            div[nonzero] = tmp_pi_i[nonzero]
-            finc = np.max( np.abs( tmp_pi_i - self.pi_i ) / div )
-            # write out progress if requested
+        if self.tagged:
+            # reset internal storage variables
+            self._f_K = None
+            self._pi_K_i = None
+            finc = None
             if verbose:
-                print " %25d %25.12e" % ( i+1, finc )
-            # break loop if we're converged
-            if finc < ftol:
-                break
-        # complain if we're not yet converged
-        if finc > ftol:
-            raise NotConvergedWarning( "DTRAM", finc )
+                print "# %25s %25s" % ( "[iteration step]", "[relative increment]" )
+            # start the iteration loop
+            for i in xrange( maxiter ):
+                # iterate nu_K_i
+                tmp_nu_K_i = np.copy( self.nu_K_i )
+                nu_K_i_equation_tagged( tmp_nu_K_i, self.gamma_K_i, self.pi_i, self.C_K_ij, self._nu_K_i )
+                # iterate pi_i
+                tmp_pi_i = np.copy( self.pi_i )
+                pi_i_equation_tagged( self.nu_K_i, self.gamma_K_i, tmp_pi_i, self.C_K_ij, self._pi_i )
+                # normalize pi_i
+                self._pi_i /= self.pi_i.sum()
+                # compute the relative change of pi_i
+                nonzero = tmp_pi_i.nonzero()
+                div = np.ones( shape=(self.n_markov_states,), dtype=np.float64 )
+                div[nonzero] = tmp_pi_i[nonzero]
+                finc = np.max( np.abs( tmp_pi_i - self.pi_i ) / div )
+                # write out progress if requested
+                if verbose:
+                    print " %25d %25.12e" % ( i+1, finc )
+                # break loop if we're converged
+                if finc < ftol:
+                    break
+            # complain if we're not yet converged
+            if finc > ftol:
+                raise NotConvergedWarning( "DTRAM", finc )
+        else:
+            pass
 
     ############################################################################
     #
@@ -146,7 +165,10 @@ class DTRAM( Estimator ):
             the transition matrices for all thermodynamic states
         """
         p_K_ij = np.zeros( shape=self.C_K_ij.shape, dtype=np.float64 )
-        p_K_ij_equation_tagged( self.nu_K_i, self.gamma_K_i, self.pi_i, self.C_K_ij, p_K_ij )
+        if self.tagged:
+            p_K_ij_equation_tagged( self.nu_K_i, self.gamma_K_i, self.pi_i, self.C_K_ij, p_K_ij )
+        else:
+            pass
         return p_K_ij
 
     def estimate_transition_matrix( self, I ):
@@ -188,5 +210,8 @@ class DTRAM( Estimator ):
 
     @property
     def gamma_K_i( self ):
-        return self._gamma_K_i
+        if self.tagged:
+            return self._gamma_K_i
+        else:
+            return None
 
