f8e3f39c6b311b2b504ee96391fe7a9f3fa79a99
Christoph Wehmeyer <christoph.wehmeyer@fu-berlin.de>
2015-05-30 23:11:00 +0200

    [READER] fixing code style

--- pytram/reader.py
+++ pytram/reader.py
@@ -2,6 +2,7 @@ r"""
 .. moduleauthor:: Christoph Wehmeyer <christoph.wehmeyer@fu-berlin.de>, Antonia Mey <antonia.mey@fu-berlin.de>
 
 """
+
 import numpy as np
 
 
@@ -12,7 +13,7 @@ import numpy as np
 #
 ####################################################################################################
 
-class Reader( object ):
+class Reader(object):
     r"""
     Parameters
     ----------
@@ -33,7 +34,8 @@ class Reader( object ):
     -----
     I import trajectories from a list of files
     """
-    def __init__( self, files, b_K_i_file=None, kT_file=None, skiprows=0, maxlength=None, verbose=False ):
+    def __init__(
+            self, files, b_K_i_file=None, kT_file=None, skiprows=0, maxlength=None, verbose=False):
         self.files = files
         self.b_K_i_file = b_K_i_file
         self.kT_file = kT_file
@@ -42,7 +44,7 @@ class Reader( object ):
         self.verbose = verbose
         self._trajs = None
         self._b_K_i = None
-        self._kT_K= None
+        self._kT_K = None
 
     ############################################################################
     #
@@ -51,35 +53,35 @@ class Reader( object ):
     ############################################################################
 
     @property
-    def trajs( self ):
+    def trajs(self):
         if self._trajs is None:
             self._trajs = []
             for f in self.files:
                 if self.verbose:
                     print "# Reading file <%s>" % f
                 try:
-                    content = np.loadtxt( f, dtype=np.float64, skiprows=self.skiprows )
-                except IOError, e:
+                    content = np.loadtxt(f, dtype=np.float64, skiprows=self.skiprows)
+                except IOError:
                     print "# ... cannot read file <%s> (ignored)" % f
                     continue
                 length = content.shape[0]
                 if self.verbose:
                     print "# ... length=%d" % length
-                if ( None is not self.maxlength ) and ( self.maxlength < length ):
+                if (self.maxlength is not None) and (self.maxlength < length):
                     length = self.maxlength
                     if self.verbose:
                         print "# ... truncating to length=%d" % self.maxlength
-                m = content[:length,0].astype( np.intc, copy=True )
-                t = content[:length,1].astype( np.intc, copy=True )
+                m = content[:length, 0].astype(np.intc, copy=True)
+                t = content[:length, 1].astype(np.intc, copy=True)
                 b = None
                 if content.shape[1] > 2:
-                    b = np.copy( content[:length,2:] )
+                    b = np.copy(content[:length, 2:])
                 if self.verbose:
                     if None == b:
                         print "# ... no energy data"
                     else:
-                        print "# ... %d energy column(s)" % ( content.shape[1] - 2 )
-                self._trajs.append( { 'm': m, 't': t, 'b': b } )
+                        print "# ... %d energy column(s)" % (content.shape[1] - 2)
+                self._trajs.append({'m': m, 't': t, 'b': b})
         return self._trajs
 
     ############################################################################
@@ -89,17 +91,18 @@ class Reader( object ):
     ############################################################################
 
     @property
-    def b_K_i( self ):
+    def b_K_i(self):
         if self._b_K_i is None:
             if self.b_K_i_file is None:
                 return None
             if self.verbose:
                 print "# Reading b_K_i_file <%s>" % self.b_K_i_file
             try:
-                self._b_K_i = np.loadtxt( self.b_K_i_file, dtype=np.float64 ).transpose().copy()
+                self._b_K_i = np.loadtxt(self.b_K_i_file, dtype=np.float64).transpose().copy()
                 if self.verbose:
-                    print "# ... found %d markov and %d thermodynamic states" % ( self._b_K_i.shape[1], self._b_K_i.shape[0] )
-            except IOError, e:
+                    print "# ... found %d markov and %d thermodynamic states" \
+                    % (self._b_K_i.shape[1], self._b_K_i.shape[0])
+            except IOError:
                 print "# ... cannot read file <%s>" % self.b_K_i_file
         return self._b_K_i
 
@@ -110,21 +113,22 @@ class Reader( object ):
     ############################################################################
 
     @property
-    def kT_K( self ):
+    def kT_K(self):
         if self._kT_K is None:
             if self.kT_file is None:
                 return None
             if self.verbose:
                 print "# Reading kT_file <%s>" % self.kT_file
             try:
-                self._kT_K = np.loadtxt( self.kT_file, dtype=np.float64 )
+                self._kT_K = np.loadtxt(self.kT_file, dtype=np.float64)
                 if self._kT_K.ndim > 1:
                     if self.verbose:
-                        print "# ... found %d columns - restricting to first column" % self._kT_K.shape[1]
-                    self._kT_K = self._kT_K[:,0].copy()
+                        print "# ... found %d columns - restricting to first column" \
+                        % self._kT_K.shape[1]
+                    self._kT_K = self._kT_K[:, 0].copy()
                 if self.verbose:
                     print "# ... found %d thermodynamic states" % self._kT_K.shape[0]
-            except IOError, e:
+            except IOError:
                 print "# ... cannot read file <%s>" % self.kT_file
         return self._kT_K
 
