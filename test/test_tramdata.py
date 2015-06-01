2a008b4995c2e1359e87facedda8cb2623f62239
Christoph Wehmeyer <christoph.wehmeyer@fu-berlin.de>
2015-06-01 15:06:47 +0200

    [TRAMDATA] fixing test file's code style

--- test/test_tramdata.py
+++ test/test_tramdata.py
@@ -11,22 +11,23 @@ from pytram import TRAMData
 import numpy as np
 
 def test_Mx_Tx_nMTstates():
-    seq = np.array( range( 20 ), dtype=np.intc )
-    trajs = [ { 'm': seq[:10], 't': seq[:10] }, { 'm': seq[10:], 't': seq[10:] } ]
-    td = TRAMData( trajs )
-    assert_true( np.all( ( seq - td.T_x ) == 0  ) )
-    assert_true( np.all( ( seq - td.M_x ) == 0  ) )
-    assert_true( td.n_markov_states == 20 )
-    assert_true( td.n_therm_states == 20 )
+    """test M_x and T_x state assignments"""
+    seq = np.array(range(20), dtype=np.intc)
+    trajs = [{'m': seq[:10], 't': seq[:10]}, {'m': seq[10:], 't': seq[10:]}]
+    td = TRAMData(trajs)
+    assert_true(np.all((seq - td.T_x) == 0))
+    assert_true(np.all((seq - td.M_x) == 0))
+    assert_true(td.n_markov_states == 20)
+    assert_true(td.n_therm_states == 20)
 
 def test_N_K_i():
+    """test states counts"""
     trajs = []
-    for K in xrange( 3 ):
-        trajs.append( {
-                'm': np.array( range( 4 ), dtype=np.intc ),
-                't': np.ones( shape=(4,), dtype=np.intc )*K
-            } )
-    td = TRAMData( trajs )
-    assert_true( np.all( td.N_K_i == 1 ) )
-    assert_true( np.all( td.N_K == 4 ) )
-    assert_true( td.N_K.sum() == 12 )
+    for K in xrange(3):
+        trajs.append({
+            'm': np.array(range(4), dtype=np.intc),
+            't': np.ones(shape=(4,), dtype=np.intc) * K})
+    td = TRAMData(trajs)
+    assert_true(np.all(td.N_K_i == 1))
+    assert_true(np.all(td.N_K == 4))
+    assert_true(td.N_K.sum() == 12)
